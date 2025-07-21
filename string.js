9a86f0b9456e9330a9e89e004b81f141c20358fc
Khush Shah <khushshah675@gmail.com>
2025-07-21 22:40:47 +0530

    Fix Redis message expiration logic in string.js

--- string.js
+++ string.js
@@ -1,7 +1,7 @@
 import client from './client.js';
 
 async function init(){
-    await client.set("msg:6","Hey from Nodejs");
+    await client.expire("msg:6","Hey from Nodejs");
     const result = await client.get("msg:6");
     console.log(result);
 }
